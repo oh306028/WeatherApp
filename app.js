91b502cb4f7c6ff599c9072d07f65aac19fa210c
Oskar <hamerlao@gmail.com>
2024-07-16 14:12:56 +0200

    Initialization

---
+++ app.js
@@ -0,0 +1,3 @@
+const API_KEY = "fd42cc713d4b3d08ef9316811052a4f8";
+
+const API_URL = `https://api.openhttps://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&appid={API key}weathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid=${API_KEY}`;
