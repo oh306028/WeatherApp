a6831350fe7db781103a852c81bd82df55c32bdc
Oskar <hamerlao@gmail.com>
2024-07-17 22:24:40 +0200

    The fetch function added, mapping the data. Doesnt work for now

--- app.js
+++ app.js
@@ -1,3 +1,33 @@
 const API_KEY = "fd42cc713d4b3d08ef9316811052a4f8";
 
-const API_URL = `https://api.openhttps://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&appid={API key}weathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid=${API_KEY}`;
+const cityInput = document.querySelector(".inputCity");
+const weatherform = document.querySelector(".weatherForm");
+const card = document.querySelector(".card");
+
+async function getData(city) {
+  const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+
+  await fetch(API_URL)
+    .then((res) => res.json())
+    .then((data) => {
+      const result = {
+        Name: data.Name,
+      };
+    });
+
+  return result;
+}
+
+function DisplayData(data) {
+  const cityName = document.createElement("h1");
+  card.appendChild(cityName);
+  cityName.textContent = data;
+}
+
+weatherform.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const city = cityInput.value;
+  const result = getData(city);
+
+  DisplayData(result);
+});
